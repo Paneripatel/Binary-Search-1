09e6185605540bde805ab5270beb75e74ad2532b
Paneri Patel <paneripatel@Paneris-MacBook-Pro-6.local>
2025-08-11 00:49:12 -0400

    Done Binary-Search-1

---
+++ SearchA2DMatrix.py
@@ -0,0 +1,32 @@
+'''
+Binary-Search-1
+
+Problem1
+
+Search a 2D Matrix(https://leetcode.com/problems/search-a-2d-matrix/)
+'''
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool: # type: ignore
+        if matrix == None or len(matrix) == 0:
+            return False
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        low = 0
+        high = m*n - 1
+
+        while low <= high:
+            mid = low + (high-low) // 2
+            row = mid//n
+            col = mid % n
+
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                low = mid+1
+            else:
+                high = mid - 1
+
+        return False            
---
+++ SearchInRotatedSortedArray.py
@@ -0,0 +1,36 @@
+'''
+Problem2
+
+Search in a Rotated Sorted Array (https://leetcode.com/problems/search-in-rotated-sorted-array/)
+'''
+
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if nums == None or len(nums) == 0:
+            return -1
+
+        n = len(nums)
+        low = 0
+        high = n-1
+        while low <= high:
+            mid = (low + high)//2
+            if nums[mid] == target:
+                return mid
+            #left sorted
+            if nums[low] <= nums[mid]:
+                #check if it lies in sorted part
+                if nums[low] <= target and nums[mid] > target:
+                    high = mid-1
+                else:
+                    low = mid+1
+            else:
+                #right sorted
+                if nums[mid] < target and nums[high] >= target:
+                    low = mid+1
+                else:
+                    high = mid - 1
+        return -1                        
+
